e5ad2dbc0f667bff75e631b6ee1dfdf39394e4a9
brol <brol31@gmail.com>
2015-04-22 16:37:03 +0200

    1.5.1

---
+++ _define.php
@@ -0,0 +1,27 @@
+<?php
+# ***** BEGIN LICENSE BLOCK *****
+#
+# This file is part of DotClear.
+#
+# Plugin Bloganniv by Francis Trautmann
+# Contributor: Pierre Van Glabeke
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE file at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# ***** END LICENSE BLOCK *****
+if (!defined('DC_RC_PATH')) { return; }
+$this->registerModule(
+	/* Name */				"Blog Anniv",
+	/* Description*/		"Counting the number of days before and after a particular date",
+	/* Author */			"Fran6t, Pierre Van Glabeke",
+	/* Version */			'1.5.1',
+	/* Properties */
+	array(
+		'permissions' => 'blogAnniv',
+		'type' => 'plugin',
+		'dc_min' => '2.7',
+		'support' => 'http://www.myouaibe.com/index.php/post/2007/08/23/Plugin-BlogAnniv-pour-DOTCLEAR-2',
+		'details' => 'http://plugins.dotaddict.org/dc2/details/bloganniv'
+		)
+);
---
+++ _widgets.php
@@ -0,0 +1,40 @@
+<?php
+# ***** BEGIN LICENSE BLOCK *****
+#
+# This file is part of DotClear.
+#
+# Plugin Bloganniv by Francis Trautmann
+# Contributor: Pierre Van Glabeke
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE file at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# ***** END LICENSE BLOCK *****
+if (!defined('DC_RC_PATH')) { return; }
+
+$core->addBehavior('initWidgets',array('blogAnnivWidgets','initWidgets'));
+
+class blogAnnivWidgets
+{
+	public static function initWidgets($w)
+	{
+		global $core;
+		$w->create('blogAnniv',__('Blog Anniv'),array('tplBlogAnniv','BlogAnnivWidget'),
+			null,
+			__('Counting the number of days before and after a particular date'));
+		$w->blogAnniv->setting('title',__('Title :'),'');
+		$w->blogAnniv->setting('ftdatecrea',__('Born Date (dd/mm/yyyy) or blank:'),'');
+		$w->blogAnniv->setting('dispyearborn',__('Display Born Date'),1,'check');
+		$w->blogAnniv->setting('dispyear',__('Display Year(s) Old'),1,'check');
+		$w->blogAnniv->setting('homeonly',__('Display on:'),0,'combo',
+			array(
+				__('All pages') => 0,
+				__('Home page only') => 1,
+				__('Except on home page') => 2
+				)
+		);
+    $w->blogAnniv->setting('content_only',__('Content only'),0,'check');
+    $w->blogAnniv->setting('class',__('CSS class:'),'');
+		$w->blogAnniv->setting('offline',__('Offline'),0,'check');
+	}
+}
